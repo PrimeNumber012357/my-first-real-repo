c156214401c0647bfe7b0d4094c52f2ddafd616e
PrimeNumber012357 <daneizitnik1@gmail.com>
2021-06-05 23:43:06 -0400

    add file to repo

---
+++ powers.py
@@ -0,0 +1,6 @@
+def x_to_y(x, y):
+    return float(x)**float(y)
+base_number = input("Enter a base number: ")
+exponent = input("Enter an exponent: ")
+print("You chose " + str(base_number) + " and raised it to a power of " + str(exponent) + " and the result is " + str(x_to_y(base_number, exponent)))
+
